5589966fa27a1304c36bf05c7c5b870d11b96f12
Examath <pauljis@outlook.com>
2023-06-30 10:18:20 +0930

    Synchronous saving

--- Utils/XML.cs
+++ Utils/XML.cs
@@ -59,17 +59,31 @@ namespace Examath.Core.Utils
         /// <typeparam name="T">The type to serialize from</typeparam>
         /// <param name="fileLocation">Location of xml file</param>
         /// <param name="data">The object to serialize</param>
+        /// <remarks>
+        /// Use <see cref="Save{T}(string, T)"/> for synchronous saving.
+        /// This method does not catch any exceptions.
+        /// </remarks>
         public static async Task SaveAsync<T> (string fileLocation, T data)
         {
             await Task.Run(() => Save(fileLocation, data));
+        }
 
-            void Save(string fileLocation, T data)
-            {
-                XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
-                using FileStream fileStream = File.Create(fileLocation);
-                using var writer = XmlWriter.Create(fileStream);
-                xmlSerializer.Serialize(writer, data);
-            }
+        /// <summary>
+        /// Serialize an object of type <typeparamref name="T"/> to an XML file synchronously
+        /// </summary>
+        /// <typeparam name="T">The type to serialize from</typeparam>
+        /// <param name="fileLocation">Location of xml file</param>
+        /// <param name="data">The object to serialize</param>
+        /// <remarks>
+        /// Use <see cref="SaveAsync{T}(string, T)"/> for asynchronous saving.
+        /// This method does not catch any exceptions.
+        /// </remarks>
+        public static void Save<T>(string fileLocation, T data)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+            using FileStream fileStream = File.Create(fileLocation);
+            using var writer = XmlWriter.Create(fileStream);
+            xmlSerializer.Serialize(writer, data);
         }
     }
 }
